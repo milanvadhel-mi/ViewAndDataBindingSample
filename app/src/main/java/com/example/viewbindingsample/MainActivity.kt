952643fc4357f43dc7088a4fc3a6d6d68ca9f033
Milan Vadhel <milan.vadhel.@mindinventory.com>
2021-08-08 05:46:37 -0700

    Normal Way with findViewById

--- app/src/main/java/com/example/viewbindingsample/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingsample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.viewbindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tvUsername: AppCompatTextView
+    lateinit var tvEmail: AppCompatTextView
+
+    //lateinit var tvEmail: Button // No type safe
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvUsername = findViewById(R.id.tvUsername)
+        tvEmail = findViewById(R.id.tvEmail)
+
+        // tvEmail = findViewById(R.id.tvSample) // No Null Safe
+
+        tvUsername.text = "Username : Milan"
+        tvEmail.text = "Email : milan.vadhel@mindinventroy.com"
     }
 }
