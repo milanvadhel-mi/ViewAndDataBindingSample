85aa24affdd130a51d1f8318f01cb90058a6c0d8
Milan Vadhel <milan.vadhel.@mindinventory.com>
2021-08-08 06:15:06 -0700

    View Binding

--- app/src/main/java/com/example/viewbindingsample/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingsample/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.viewbindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewbindingsample.databinding.ActivityMainBinding
 
+/**
+ * Type safe
+ * Null safe
+ * */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            tvUsername.text = "Username : Milan Vadhel"
+            tvEmail.text = "milan.vadhel@mindinventroy.com"
+            //tvSample.text = "Hello World" // Null Safe
+        }
     }
 }
---
+++ app/src/main/java/com/example/viewbindingsample/SampleFragment.kt
@@ -0,0 +1,33 @@
+package com.example.viewbindingsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.viewbindingsample.databinding.FragmentSampleBinding
+
+
+class SampleFragment : Fragment() {
+
+    private var _binding: FragmentSampleBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSampleBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    /**
+     * Fragments outlive their views.
+     * Make sure you clean up any references to the binding class instance in the fragment's onDestroyView() method.
+     * */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
