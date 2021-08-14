ce7ece20e56739ed1c056a01a098334daf9221f7
Milan Vadhel <milan.vadhel.@mindinventory.com>
2021-08-13 22:57:00 -0700

    Kotlin Synthetic

--- app/src/main/java/com/example/viewbindingsample/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingsample/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.viewbindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.second_layout.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvUsername.text = "Milan Kumar"
+        // tvCompanyName.text = "Milan Kumar" // No Null Safety
+        tvEmail.text = "milan.vadhel@mindinventory.com"
     }
 }
